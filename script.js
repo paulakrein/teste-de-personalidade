90fb2fc7fcc2268ff30a1ad0c52025351dd1e731
paulakrein <kreinpaula@gmail.com>
2025-03-15 13:04:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+window.nextQuestion = nextQuestion;
+window.prevQuestion = prevQuestion;
+window.goToQuestion = goToQuestion;
+window.submitQuiz = submitQuiz;
+
 const questions = [
     "Pergunta 1", "Pergunta 2", "Pergunta 3", "Pergunta 4", "Pergunta 5", 
     "Pergunta 6", "Pergunta 7", "Pergunta 8", "Pergunta 9", "Pergunta 10"
